52ba4e9f6e44f58251f896c2e03b8ebe30958500
ChynChyngyz <chyngyz.baybosunov@mail.ru>
2026-04-10 11:32:50 +0600

    q

--- back/back/settings.py
+++ back/back/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "*",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:62225",
